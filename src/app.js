dd2b859aee600a925422bc568ed6a32c4f1f296a
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-02-21 15:40:39 +0300

    add footer

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
 import './main.scss'
 
-console.log('portfolio')
+document.addEventListener('DOMContentLoaded', () => {
+    // form callback
+    const formCallbackInput = document.querySelectorAll('.form-callback__input, .form-callback__textarea')
+
+    formCallbackInput.forEach($el => {
+      $el.addEventListener('click', e => {
+        e.preventDefault()
+        $el.parentElement.querySelector('.fake-placeholder').classList.add('form-callback__fake-placeholder-active')
+      })
+
+      $el.addEventListener('blur', e => {
+        e.preventDefault()
+        if ($el.value.trim() === '') {
+          $el.parentElement.querySelector('.fake-placeholder').classList.remove('form-callback__fake-placeholder-active')
+        }
+      })
+    })
+
+    const formCallbackSubmit = document.getElementById('form-callback__submit')
+    formCallbackSubmit.addEventListener('click', e => {
+      e.preventDefault()
+    })
+  }
+)
